bc53ffcfcbc8411a17616fcea46da7e8c708c424
Generic35 <ricklamontagne@gmail.com>
2018-04-22 18:45:07 -0400

    add and use the loadCourses thunk on app load

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
-import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
+
+import configureStore from './store/configureStore';
+import {loadCourses} from './actions/courseActions';
+
 import './styles/styles.css'; //Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
-
+store.dispatch(loadCourses());
 render(
   <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
